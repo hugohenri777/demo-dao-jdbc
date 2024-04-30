8c1afc9220895eb818f2073c6a4ffde8b2982b4a
Hugo Oliveira <hugohenri777@gmail.com>
2024-04-29 21:56:29 -0300

    Reusing Instatiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,6 +13,8 @@ public class Program {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		System.out.println("### Test1: Seller findById ###");
+		
 		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
