1aa37a13d1e80627c82c219fa8a595bf1a6220c8
Hugo Oliveira <hugohenri777@gmail.com>
2024-05-06 21:24:22 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,6 +27,13 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n### Test3: Seller findAll ###");
+		
+		list = sellerDao.findAll();
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
